40f22d18c3770345c50cd256e0f1b2ce7e842c12
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2020-10-26 10:31:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Weijie, Jiang."
+let userName = "weijiej2964";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
